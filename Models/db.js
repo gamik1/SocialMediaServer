489ed4bbdcebcafce6ff10a74d22540176dd0165
Gamik Budhathoki <gamiksingz@gmail.com>
2022-10-10 15:08:20 -0400

    Added Passport  Passport-JWT login and signup routes

--- Models/db.js
+++ Models/db.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 
 mongoose.connect(`${process.env.MONGO_DB}`, {useNewUrlParser: true});
+mongoose.connection.on('error', error => console.log(error) );
 mongoose.connection.on("connected", function(){    
     console.log("server is connected to Databse");
-})
+});
+mongoose.Promise = global.Promise;
--- server.js
+++ server.js
@@ -6,15 +6,21 @@ const dotenv = require('dotenv');
 const helmet = require("helmet"); //Helmet helps you secure your Express apps by setting various HTTP headers.
 const morgan = require('morgan'); // logger
 require('./Models/db');
-
-
+const passport = require('passport');
+const routes = require('./routes/routes');
+const secureRoute = require('./routes/secure-routes');
+const UserModel = require('./Models/User');
 const app = express();
-const port = 3000;
+const port = 8800;
+
+require("./Auth/auth");
 
 
 
 app.use(cors());
+app.use(express.json());
 app.use(helmet());
+app.use(morgan("common"));
 
 dotenv.config();
 
@@ -22,12 +28,16 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/', routes);
 
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
 
-app.get('/', (req, res) => {
-    res.send("<h1>This is server</h1>");
-});
-
+// Handle errors.
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json({ error: err });
+// });
 
 
 app.listen(port, ()=> console.log(`Social Media Server Started -- on port ${port}!`));
